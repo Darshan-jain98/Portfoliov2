d94f02767f803a5ac25d12e53b4e66d5c115c006
Darshan-jain98 <darshan.jain252@gmail.com>
2023-02-28 19:57:15 -0500

    Updated the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.svg',
+        icon: 'src/images/Logo.svg',
       },
     },
     `gatsby-plugin-offline`,
